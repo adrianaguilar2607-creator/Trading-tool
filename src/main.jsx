eae4ebbf048bfd36db4a57051d83b19434cdf7b5
adrianaguilar2607-creator <adrianaguilar.2607@gmail.com>
2026-04-21 17:53:44 +0200

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import TradingTool from "./TradingTool.jsx"
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <TradingTool />
+  </React.StrictMode>,
+)
