317e92b4739cd07b570137171dd7b6c277f8ebe8
adrianaguilar2607-creator <adrianaguilar.2607@gmail.com>
2026-04-21 18:08:19 +0200

    add express server

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+
+const app = express()
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+app.use(express.static(join(__dirname, 'dist')))
+app.get('*', (req, res) => {
+  res.sendFile(join(__dirname, 'dist', 'index.html'))
+})
+
+app.listen(process.env.PORT || 3000)
